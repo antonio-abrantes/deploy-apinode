5e7ccb6a61098c16b9488cc6c7b9f8a3aaf330f5
tonhus <antonio_abrantes@hotmail.com.br>
2020-06-15 17:29:01 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Database setup
+mongoose.connect(
+  process.env.MONGO_URL, {
+  useNewUrlParser: true,
+});
+
+// require('./src/models/Product');
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3333, () => {
+  // console.log('Server on port:3333');
+});
